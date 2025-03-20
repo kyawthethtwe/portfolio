cea691985af4e2ac0c26e4cec2f5b4486cd07f40
Jo <kyawthethtwe595@gmail.com>
2025-03-21 01:10:23 +0700

    add analytics

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
@@ -53,6 +54,7 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
         <StructuredData />
+        <Analytics />
       </body>
     </html>
   );
